92f59bafbf7a208ef673df3e4301ad88d7ab51a0
sudheer368 <sudheersunny369@gmail.com>
2024-09-25 18:11:16 +0530

    add

--- lib/register.dart
+++ lib/register.dart
@@ -1,17 +1,5 @@
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(MyApp());
-}
-
-class MyApp extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      home: RegistrationScreen(),
-    );
-  }
-}
 
 class RegistrationScreen extends StatefulWidget {
   @override
